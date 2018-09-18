df9142e2ba42567e71c659bfbd19712ded971f80
yang <1823029696@qq.com>
2018-09-18 17:37:24 +0800

    add

--- shop/admin/src/pages/todolist/store/actionType.js
+++
@@ -1,5 +0,0 @@
-
-export const CHANGE_VALUE = 'change_value';
-export const ADD_ITEM = 'add_item';
-export const DELETE_ITEM = 'delete_item';
-export const GET_LIST_INIT_DATA = 'get_list_init_data';
--- shop/admin/src/store/reducer.js
+++
@@ -1,7 +0,0 @@
-//把store生成immutable数据	
-import { combineReducers } from 'redux-immutable';
-import { reducer as todolistReducer } from  '../pages/todolist/store/index.js';
-
-export default combineReducers({
-	todolist:todolistReducer
-})
